601c4cc24794ea7b64ed988a3b34fd47372c363b
zhangting2020 <zhangting_2017@163.com>
2017-12-19 18:43:41 +0800

    序列化二叉树

---
+++ JianZhiOffer/46.序列化二叉树.cpp
@@ -0,0 +1,61 @@
+/************************************************************************************************************
+题目描述
+请实现两个函数，分别用来序列化和反序列化二叉树
+解题思路：
+1.序列化：先序遍历树的结点，遇到空结点，设为‘#’。
+2.反序列化：遍历字符串，以先序的模式重构树
+*************************************************************************************************************/
+class Solution {  
+public:
+    char* Serialize(TreeNode *root) {
+       if(root == NULL)
+           return NULL;
+        string str;
+        Serialize(root, str);
+        char *ret = new char[str.length() + 1];  //须是指向堆内存的指针，否则该段程序结束，返回时指针就会失效
+        int i;
+        for(i = 0; i < str.length(); i++){
+            ret[i] = str[i];
+        }
+        ret[i] = '\0';
+        return ret;
+    }
+    void Serialize(TreeNode *root, string& str){
+        if(root == NULL){
+            str += '#';
+            return ;
+        }
+        string r = to_string(root->val);
+        str += r;
+        str += ',';
+        Serialize(root->left, str);
+        Serialize(root->right, str);
+    }
+     
+    TreeNode* Deserialize(char *str) {
+        if(str == NULL)
+            return NULL;
+        TreeNode *ret = Deserialize(&str);
+ 
+        return ret;
+    }
+    TreeNode* Deserialize(char **str){//由于递归时，会不断的向后读取字符串
+        if(**str == '#'){  //所以一定要用**str,
+            ++(*str);         //以保证得到递归后指针str指向未被读取的字符
+            return NULL;
+        }
+        int num = 0;
+        while(**str != '\0' && **str != ','){
+            num = num*10 + ((**str) - '0');
+            ++(*str);
+        }
+        TreeNode *root = new TreeNode(num);
+        if(**str == '\0')
+            return root;
+        else
+            (*str)++;
+        root->left = Deserialize(str);
+        root->right = Deserialize(str);
+        return root;
+    }
+};
